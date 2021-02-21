4e7f8d95a2adca15d19487f6b9a95ee722140ade
Hafez <hafezusman98@gmail.com>
2021-02-21 21:39:15 +0400

    Changes in App.js component for App component inline styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Bio from './components/Bio'
 import Sliders from './components/Sliders'
-import Wallpaper from './Constellation_BG.png'
+//import Wallpaper from './Constellation_BG.png'
 
 function App() {
   return (
-    <div className="App" style={{maxHeight: '100vh', background: {Wallpaper}}}>
+    <div className="App">
       <Bio />
       <Sliders/>
     </div>
